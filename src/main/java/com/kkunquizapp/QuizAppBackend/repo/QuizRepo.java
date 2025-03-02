c3c8d6e26f26a33f5fec227eccb7873fbf8ba189
thaihoan2703 <hoanthai1103@gmail.com>
2025-03-02 13:44:15 +0700

    quiz get data

--- src/main/java/com/kkunquizapp/QuizAppBackend/repo/QuizRepo.java
+++ src/main/java/com/kkunquizapp/QuizAppBackend/repo/QuizRepo.java
@@ -4,10 +4,11 @@ import com.kkunquizapp.QuizAppBackend.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QuizRepo extends JpaRepository<Quiz, UUID> {
-
+    List<Quiz> findByHost_UserId(UUID userId);
 }
